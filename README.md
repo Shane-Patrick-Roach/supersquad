9057e7611ba900df00ddb1386d86e67117c28d47
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2022-05-05 15:53:49 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# SuperSquad
+
+An application that allows you to add different superheroes to your dream team.
+
+
+## Tools Used
+
+- React
+- Redux
+- Bootstrap
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import CharacterList from "./CharacterList";
+
+class App extends Component {
+  render() {
+    return (
+      <div>SuperSquad
+        <CharacterList />
+      </div>
+      
+    )
+  }
+}
+
+export default App;
